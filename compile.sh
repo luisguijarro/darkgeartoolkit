b6010685f629013786a38c7ba2eb9a5aabded2a5
Luis Guijarro Pérez <luisgp82@hotmail.es>
2020-09-06 03:57:59 +0200

    Añadimos Hora de ejecucion y simbolos DEBUG.

--- compile.sh
+++ compile.sh
@@ -18,16 +18,33 @@ makedir()
     fi
 }
 
+nspaces()
+{
+    widthterminal=$(($(stty size | cut '-d ' -f2)))
+    spcs=$(($widthterminal-44))
+    spcschar=' '
+    ret=''
+    spcscont=0
+    while [ $spcscont -lt $spcs ]
+    do
+        ret="${ret}${spcschar}"
+        spcscont=$(($spcscont+1))
+    done
+
+    echo "${ret}";  
+}
+
 showhead()
 {
     echo ''
     echo "\e[1m\e[38;2;255;128;128mDark Gear Tool Kit compiler script."
     echo "\e[38;2;128;128;255m"$barra
+    espacios=$(nspaces)
     if [ "$1" = "-d" ]
-    then
-        echo "\e[0mCompiling in Debug Mode."
+    then        
+        echo "\e[0mCompiling in Debug Mode. ${espacios}$(date +"%d-%m-%Y %H:%M:%S")"
     else
-        echo "\e[0mCompiling in Normal Mode."
+        echo "\e[0mCompiling in Normal Mode.${espacios}$(date +"%d-%m-%Y %H:%M:%S")"
     fi
 
     echo "\e[1m\e[38;2;128;128;255m"$barra"\e[0m"
@@ -49,7 +66,7 @@ case "$1" in
     makedir "./bin"
     makedir "./bin/Debug"
     cp dgtk.dll.config ./bin/Debug/
-    csc -nologo -unsafe -target:library -debug:full -recurse:./*.cs -out:./bin/Debug/dgtk.dll
+    csc -nologo -unsafe -target:library -debug:full -define:DEBUG -recurse:./*.cs -out:./bin/Debug/dgtk.dll
     break
     ;;
     *)
