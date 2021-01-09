197eb63e396b767dee31400d446971ee195f484a
Luis Guijarro Pérez <luisgp82@hotmail.es>
2021-01-09 02:21:51 +0100

    Obtenemos punteros a delegados de control de VSync en la comprobacion.

--- Platforms/X11/V-Sync.cs
+++ Platforms/X11/V-Sync.cs
@@ -9,14 +9,17 @@ namespace dgtk.Platforms.X11
             string s_ext = new string ((sbyte*)glx.glXQueryExtensionsString(display, idScreen));
             if (s_ext.Contains("GLX_EXT_swap_control"))
             {
+                dgtk.Platforms.X11.glx.glXSwapIntervalEXT = (dgtk.Platforms.X11.glx.d_glXSwapIntervalEXT)dgtk.OpenGL.InternalTool.GetGLMethodAdress("glXSwapIntervalEXT", typeof(dgtk.Platforms.X11.glx.d_glXSwapIntervalEXT));
                 return LinuxSwapControlExt.GLX_EXT_swap_control;
             }
             if (s_ext.Contains("GLX_MESA_swap_control"))
             {
+                dgtk.Platforms.X11.glx.glXSwapIntervalMESA = (dgtk.Platforms.X11.glx.d_glXSwapIntervalMESA)dgtk.OpenGL.InternalTool.GetGLMethodAdress("glXSwapIntervalMESA", typeof(dgtk.Platforms.X11.glx.d_glXSwapIntervalMESA));
                 return LinuxSwapControlExt.GLX_MESA_swap_control;
             }
             if (s_ext.Contains("GLX_SGI_swap_control"))
             {
+                dgtk.Platforms.X11.glx.glXSwapIntervalSGI = (dgtk.Platforms.X11.glx.d_glXSwapIntervalSGI)dgtk.OpenGL.InternalTool.GetGLMethodAdress("glXSwapIntervalSGI", typeof(dgtk.Platforms.X11.glx.d_glXSwapIntervalSGI));
                 return LinuxSwapControlExt.GLX_SGI_swap_control;
             }
             return LinuxSwapControlExt.V_SyncNoSupported;
