08704e7d38caccf39d7858f876fdca3f38489a3f
Luis Guijarro Pérez <luisgp82@hotmail.es>
2021-01-09 21:55:59 +0100

    Comentamos excepcion de wglChoosePixelFormatARB por que en algunos equipos siempre devuelve false.

--- Platforms/Win32/OpenGLPreparation.cs
+++ Platforms/Win32/OpenGLPreparation.cs
@@ -80,8 +80,10 @@ namespace dgtk.Platforms.Win32
 			uint numFormats;
             if (!wglChoosePixelFormatARB(DeviceC, ref attribList, IntPtr.Zero, 1, out pixelFormat, out numFormats))
             {
-				error = Marshal.GetLastWin32Error();
-                throw new Exception("wglChoosePixelFormatARB FAIL!!! -> " + new Win32Exception(error).Message);
+				// Comentamos por que en algunos equipos siempre devuelve false;
+
+				//error = Marshal.GetLastWin32Error();
+                //throw new Exception("wglChoosePixelFormatARB FAIL!!! -> " + new Win32Exception(error).Message);
             }
         }
 
